70fcdfe3518e6475ab26d14e3d6661b636b21ab5
edangx100 <edangx100@gmail.com>
2022-02-17 16:07:16 -0800

    add Procfile for Heroku use

---
+++ check_heroku.py
@@ -0,0 +1,21 @@
+import requests
+
+
+data = {
+    "age": 40,
+    "workclass": "Private",
+    "education": "Masters",
+    "maritalStatus": "Married-civ-spouse",
+    "occupation": "Exec-managerial",
+    "relationship": "Husband",
+    "race": "White",
+    "sex": "Male",
+    "hoursPerWeek": 60,
+    "nativeCountry": "United-States"
+    }
+r = requests.post('https://proj3-mlops.herokuapp.com/', json=data)
+
+assert r.status_code == 200
+
+print(f"Response status code: {r.status_code}")
+print(f"Model inference result: {r.json()}")
